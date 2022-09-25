3ab613e3806b9d86211b22e661c260ad942de1cd
Hemantdhurve <hemantdhurve007@gmail.com>
2022-09-26 00:20:50 +0530

    [Hemant] Created EmpPayoll_Service Database

---
+++ ADO.net_EmpPayrollService/EmpRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.net_EmpPayrollService
+{
+    public class EmpRepository
+    {
+        //UC1 Create Database
+        public static void CreateDataBase()
+        {
+
+            SqlConnection con = new SqlConnection(@"Data Source=ITS-HEMANT-PC\SQLEXPRESS; Initial Catalog=master;Integrated Security=True");
+
+            try
+            {
+                //Using block is used to close the connection without using close property
+                using(con)
+                {
+                    //Writing SQL query
+                    string query = "Create database EmpPayroll_Service";
+                    SqlCommand cmd = new SqlCommand(query, con);
+
+                    //Opening Connection
+                    con.Open();
+                    //Executing SQL query
+                    cmd.ExecuteNonQuery();
+
+                    Console.WriteLine("Database Created Successfully");
+
+                }
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Something Went Wrong..."+e.Message);
+            }
+
+        }
+    }
+}
+
+       
+   
--- ADO.net_EmpPayrollService/Program.cs
+++ ADO.net_EmpPayrollService/Program.cs
@@ -7,6 +7,25 @@ namespace ADO.net_EmpPayrollService
         public static void Main()
         {
             Console.WriteLine("Welcome to Employee Payroll Service ADO.Net");
+
+            Console.WriteLine("Please Select the Options:\n " +
+                "1) Database Creation\n");
+
+            int option=Convert.ToInt32(Console.ReadLine());
+
+            EmpModel model = new EmpModel();
+            EmpRepository repo = new EmpRepository();
+            switch (option)
+            {
+                case 1:
+                    EmpRepository.CreateDataBase();
+                    break;
+
+                default:
+                    Console.WriteLine("Please Enter Proper Option");
+                    break;
+
+            }
         }
     }
 }
