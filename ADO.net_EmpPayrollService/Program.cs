e6a5a7bf716eeed1cbf0b6f448ea0970d3795fa5
Hemantdhurve <hemantdhurve007@gmail.com>
2022-09-25 21:47:53 +0530

    Welcome Message

---
+++ ADO.net_EmpPayrollService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADO.net_EmpPayrollService
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Employee Payroll Service ADO.Net");
+        }
+    }
+}
